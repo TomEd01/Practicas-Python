cd93ffea71e517c20ee3fe25029a2619e1b4bb62
TomiWebsite <tomicatwebsite@gmail.com>
2022-03-12 12:32:02 -0500

    Datos en python

---
+++ datos.py
@@ -0,0 +1,27 @@
+'''
+            VARIABLES
+    °No pueden empezar con numeros pero si con gion bajo (en casos especiales)
+    °Solo pueden contener caractares afanumericos (A-Z,a-z,0-9,_)
+    °Se distingue entre mayuscula y minuscula
+'''
+print('\n')
+edad = 24 #Tipo de dato entero
+Edad = 19
+EDAD = 33
+print(type(edad))
+print(edad,Edad,EDAD)
+#--------------------------#
+print('\n')
+flo = 5.5#Tipo de dato flotante = decimal
+print(type(flo))
+print(flo)
+#--------------------------#
+print('\n')
+nombre = 'Edwin Tomas / TomiCat'#Tipo String = cadena de caracteres
+print(type(nombre))
+print(nombre)
+#--------------------------#
+print('\n')
+bo = True#Tipo de dato boleano
+print(type(bo))
+print(bo)
